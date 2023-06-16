83f145797b16814a7ff5785d87f1ab0294dc29cf
killshotxd <hassanansari211@gmail.com>
2023-06-16 14:22:02 +0530

    Task Create API

--- pages/api/newtask.js
+++ pages/api/newtask.js
@@ -1,15 +1,25 @@
 import { connectDb } from "../../utils/features";
 import { Task } from "../../models/task";
+
 const handler = async (req, res) => {
+  if (req.method !== "POST")
+    return res.status(400).json({
+      success: false,
+      message: "Only POST method is Allowed",
+    });
+
   await connectDb();
 
+  const { title, description } = req.body;
+
   await Task.create({
-    title: "Sample Title",
-    description: "Sample Des",
+    title,
+    description,
     user: "adsd",
   });
   res.json({
     success: true,
+    message: "Task Created !",
   });
 };
 
